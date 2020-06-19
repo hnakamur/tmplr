4a6f9a5ddf0e60cef7356843f5e6cbcc06495eba
Hiroaki Nakamura <hnakamur@gmail.com>
2020-06-19 23:38:22 +0900

    Add -ref and -ref-recursive option

--- cmd/tmplr/main.go
+++ cmd/tmplr/main.go
@@ -3,18 +3,52 @@ package main
 import (
 	"flag"
 	"log"
+	"strings"
 
 	"github.com/hnakamur/tmplr"
 )
 
+type stringFlags []string
+
+func (ss *stringFlags) String() string {
+	if ss == nil {
+		return ""
+	}
+
+	var b strings.Builder
+	for i, s := range *ss {
+		if i > 0 {
+			b.WriteString(" ")
+		}
+		b.WriteString(s)
+	}
+	return b.String()
+}
+
+func (ss *stringFlags) Set(value string) error {
+	*ss = append(*ss, value)
+	return nil
+}
+
 func main() {
 	varFilename := flag.String("var", "var.yml", "variable YAML file")
-	destFilename := flag.String("dest", "", "destination file (\"\" or \"-\" means stdout)")
+	destFilename := flag.String("dest", "", "destination file (\"\" means stdout)")
 	tmplName := flag.String("name", "main", "template name to execute")
-	tmplPattern := flag.String("template", "", "template filename glob pattern (\"\" or \"-\" means stdin)")
+	tmplPattern := flag.String("tmpl", "", "template filename glob pattern (\"\" means stdin)")
+	var yamlRefDirs stringFlags
+	flag.Var(&yamlRefDirs, "ref", "variable YAML reference directory (can specify multiple times)")
+	refRecursive := flag.Bool("ref-recursive", true, "searches yaml file recursively in -ref directories")
 	flag.Parse()
 
-	err := tmplr.Run(*destFilename, *varFilename, *tmplName, *tmplPattern)
+	cfg := &tmplr.Config{
+		DestFilename:     *destFilename,
+		TemplateName:     *tmplName,
+		TemplatePattern:  *tmplPattern,
+		VarFilename:      *varFilename,
+		YAMLRefDirs:      []string(yamlRefDirs),
+		YAMLRefRecursive: *refRecursive,
+	}
+	err := tmplr.Run(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
--- tmplr.go
+++ tmplr.go
@@ -11,16 +11,27 @@ import (
 	"github.com/goccy/go-yaml"
 )
 
-func Run(destFilename, varFilename, tmplName, tmplPattern string) (err error) {
+type Config struct {
+	DestFilename string
+
+	TemplateName    string
+	TemplatePattern string
+
+	VarFilename      string
+	YAMLRefDirs      []string
+	YAMLRefRecursive bool
+}
+
+func Run(cfg *Config) (err error) {
 	var data interface{}
-	data, err = ReadYAMLFile(varFilename)
+	data, err = readYAMLFile(cfg.VarFilename, cfg.YAMLRefDirs, cfg.YAMLRefRecursive)
 	if err != nil {
 		return err
 	}
 
-	base := template.New(tmplName).Funcs(sprig.TxtFuncMap())
+	base := template.New(cfg.TemplateName).Funcs(sprig.TxtFuncMap())
 	var tmpl *template.Template
-	if tmplPattern == "" || tmplPattern == "-" {
+	if cfg.TemplatePattern == "" {
 		var data []byte
 		data, err = ioutil.ReadAll(os.Stdin)
 		if err != nil {
@@ -31,18 +42,18 @@ func Run(destFilename, varFilename, tmplName, tmplPattern string) (err error) {
 			return err
 		}
 	} else {
-		tmpl, err = base.ParseGlob(tmplPattern)
+		tmpl, err = base.ParseGlob(cfg.TemplatePattern)
 		if err != nil {
 			return err
 		}
 	}
 
 	var w io.Writer
-	if destFilename == "" || destFilename == "-" {
+	if cfg.DestFilename == "" {
 		w = os.Stdout
 	} else {
 		var file *os.File
-		file, err = os.Create(destFilename)
+		file, err = os.Create(cfg.DestFilename)
 		if err != nil {
 			return err
 		}
@@ -59,7 +70,7 @@ func Run(destFilename, varFilename, tmplName, tmplPattern string) (err error) {
 		w = bw
 	}
 
-	err = tmpl.ExecuteTemplate(w, tmplName, data)
+	err = tmpl.ExecuteTemplate(w, cfg.TemplateName, data)
 	if err != nil {
 		return err
 	}
@@ -67,14 +78,16 @@ func Run(destFilename, varFilename, tmplName, tmplPattern string) (err error) {
 	return nil
 }
 
-func ReadYAMLFile(filename string) (interface{}, error) {
+func readYAMLFile(filename string, refDirs []string, refRecursive bool) (interface{}, error) {
 	file, err := os.Open(filename)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	d := yaml.NewDecoder(bufio.NewReader(file))
+	d := yaml.NewDecoder(bufio.NewReader(file),
+		yaml.ReferenceDirs(refDirs...),
+		yaml.RecursiveDir(refRecursive))
 	var v interface{}
 	if err := d.Decode(&v); err != nil {
 		return nil, err
