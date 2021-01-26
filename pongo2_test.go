625f4087e212a8b22de713e6c7a58fe07c8107c4
Hiroaki Nakamura <hnakamur@gmail.com>
2021-01-26 12:51:49 +0900

    Add failing test for pongo2 escape

---
+++ pongo2_test.go
@@ -0,0 +1,22 @@
+package tmplr
+
+import (
+	"testing"
+
+	"github.com/flosch/pongo2/v4"
+)
+
+func TestPongo2AutoEscape(t *testing.T) {
+	tpl, err := pongo2.FromString("{{ arg }}")
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := tpl.Execute(pongo2.Context{"arg": "'--extra-vars=\"var1=value\"'"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "'--extra-vars=\"var1=value\"'"
+	if got != want {
+		t.Errorf("result unmatch, got=%s, want=%s", got, want)
+	}
+}
