c4fc0ae77ceeb2d59c830860ab4ca1e7483f6d7e
Hiroaki Nakamura <hnakamur@gmail.com>
2023-06-10 16:09:53 +0900

    Show version from build info

--- cmd/tmplr/main.go
+++ cmd/tmplr/main.go
@@ -4,13 +4,41 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
+	"runtime/debug"
 	"strings"
 
 	"github.com/hnakamur/tmplr"
 )
 
-var version string = "0.5.0"
+func main() {
+	varFilename := flag.String("var", "var.yml", "variable YAML file")
+	destFilename := flag.String("dest", "", "destination file (\"\" means stdout)")
+	tmplFilename := flag.String("tmpl", "", "template filename to execute")
+	showVersion := flag.Bool("version", false, "show version and exit")
+
+	var yamlRefDirs stringFlags
+	flag.Var(&yamlRefDirs, "ref", "variable YAML reference directory (can specify multiple times)")
+
+	refRecursive := flag.Bool("ref-recursive", true, "searches yaml file recursively in -ref directories")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(Version())
+		return
+	}
+
+	cfg := &tmplr.Config{
+		DestFilename:     *destFilename,
+		TemplateFilename: *tmplFilename,
+		VarFilename:      *varFilename,
+		YAMLRefDirs:      []string(yamlRefDirs),
+		YAMLRefRecursive: *refRecursive,
+	}
+	err := tmplr.Run(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type stringFlags []string
 
@@ -34,32 +62,10 @@ func (ss *stringFlags) Set(value string) error {
 	return nil
 }
 
-func main() {
-	varFilename := flag.String("var", "var.yml", "variable YAML file")
-	destFilename := flag.String("dest", "", "destination file (\"\" means stdout)")
-	tmplFilename := flag.String("tmpl", "", "template filename to execute")
-	showVersion := flag.Bool("version", false, "show version and exit")
-
-	var yamlRefDirs stringFlags
-	flag.Var(&yamlRefDirs, "ref", "variable YAML reference directory (can specify multiple times)")
-
-	refRecursive := flag.Bool("ref-recursive", true, "searches yaml file recursively in -ref directories")
-	flag.Parse()
-
-	if *showVersion {
-		fmt.Println(version)
-		os.Exit(0)
+func Version() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "(devel)"
 	}
-
-	cfg := &tmplr.Config{
-		DestFilename:     *destFilename,
-		TemplateFilename: *tmplFilename,
-		VarFilename:      *varFilename,
-		YAMLRefDirs:      []string(yamlRefDirs),
-		YAMLRefRecursive: *refRecursive,
-	}
-	err := tmplr.Run(cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return info.Main.Version
 }
