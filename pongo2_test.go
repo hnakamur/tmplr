d83a7a8d0c6c84e9e3709e7e3b10ce132758d525
Hiroaki Nakamura <hnakamur@gmail.com>
2023-06-10 16:00:10 +0900

    Add failing test for case error discarded

--- pongo2_test.go
+++ pongo2_test.go
@@ -1,6 +1,8 @@
 package tmplr
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/flosch/pongo2/v6"
@@ -19,5 +21,36 @@ func TestPongo2AutoEscape(t *testing.T) {
 	want := "'--extra-vars=\"var1=value\"'"
 	if got != want {
 		t.Errorf("result unmatch, got=%s, want=%s", got, want)
+	}
+}
+
+func TestErrNotDiscardedForFileOutput(t *testing.T) {
+	dir := t.TempDir()
+	destFilename := filepath.Join(dir, "vanish.md")
+	tmplFilename := filepath.Join(dir, "vanish.j2")
+	varFilename := filepath.Join(dir, "vars.yaml")
+
+	const varsStr = `a: b`
+	const tmplStr = `{% macro show_zone(zone) %}
+	zone is {{ zone }}
+	{% endmacro %}
+
+	{{ show_zone("osk", "foo") }}
+`
+
+	if err := os.WriteFile(tmplFilename, []byte(tmplStr), 0o644); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(varFilename, []byte(varsStr), 0o644); err != nil {
+		t.Fatal(err)
+	}
+
+	cfg := &Config{
+		DestFilename:     destFilename,
+		TemplateFilename: tmplFilename,
+		VarFilename:      varFilename,
+	}
+	if err := Run(cfg); err == nil {
+		t.Errorf("should got an error, but got no error")
 	}
 }
